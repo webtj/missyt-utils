7a0917291775c885d276c479e243e78eb4b268e0
怕疼的猫 <taojian@petalmail.com>
2022-11-20 02:42:35 +0800

    新增浏览器事件注册和注销

---
+++ src/browser/event.ts
@@ -0,0 +1,62 @@
+type ElementTypes = {
+  addEventListener: (
+    type: string,
+    listener: EventListenerOrEventListenerObject,
+    options?: boolean | AddEventListenerOptions,
+  ) => void;
+  removeEventListener: (
+    type: string,
+    listener: EventListenerOrEventListenerObject,
+    options?: boolean | EventListenerOptions,
+  ) => void;
+  attachEvent: (type: string, listener: EventListenerOrEventListenerObject) => void;
+  detachEvent: (type: string, listener: EventListenerOrEventListenerObject) => void;
+};
+
+const on = <T extends ElementTypes>(
+  el: T,
+  type: string,
+  listener: EventListenerOrEventListenerObject,
+  options?: boolean | AddEventListenerOptions,
+) => {
+  if (el.addEventListener) {
+    el.addEventListener(type, listener, options);
+  } else if (el.attachEvent) {
+    el.attachEvent(`on${type}`, listener);
+  } else {
+    throw new Error('Your browser does not support addEventListener');
+  }
+};
+
+const off = <T extends ElementTypes>(
+  el: T,
+  type: string,
+  listener: EventListenerOrEventListenerObject,
+  options?: boolean | EventListenerOptions,
+) => {
+  if (el.removeEventListener) {
+    el.removeEventListener(type, listener, options);
+  } else if (el.detachEvent) {
+    el.detachEvent(`on${type}`, listener);
+  } else {
+    throw new Error('Your browser does not support removeEventListener');
+  }
+};
+
+/**
+ * @var {object} event
+ * @description 事件对象
+ * @property {function} on 绑定事件
+ * @property {function} off 解绑事件
+ * @example
+ * import { event } from 'missyt-utils';
+ * event.on(document.querySelector('.col'), 'click', () => {
+ *  console.log('click');
+ * });
+ * event.off(document.querySelector('.col'), 'click', () => {
+ * console.log('click');
+ * });
+ */
+const event = { on, off };
+
+export { event };
--- src/browser/index.ts
+++ src/browser/index.ts
@@ -2,3 +2,5 @@ export * from './browser';
 export * from './device';
 export * from './dom';
 export * from './url';
+export * from './event';
+export * from './notification';
