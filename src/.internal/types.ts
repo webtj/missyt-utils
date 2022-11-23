5d8d65ede5cac4242d85791be86a45f67fe24588
怕疼的猫 <taojian@petalmail.com>
2022-11-23 23:51:09 +0800

    增加函数类型定义

--- src/.internal/types.ts
+++ src/.internal/types.ts
@@ -1,4 +1,5 @@
 type EmptyFunction = () => void;
+type paramsFunction = (param: any, ...arg: []) => void;
 type StringOrother<T> = T extends number ? string : T;
 type NumberOrother<T> = T extends string ? number : T;
-export { EmptyFunction, StringOrother, NumberOrother };
+export { EmptyFunction, paramsFunction, StringOrother, NumberOrother };
